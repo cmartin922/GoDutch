006076397a3b6c136d6b51a06addb5d75203aad9
saragong14 <sarasgong@gmail.com>
2016-11-12 01:12:02 -0800

    Added checkboxes for selector

--- app/src/main/java/com/hackfa16/godutch/Selector.java
+++ app/src/main/java/com/hackfa16/godutch/Selector.java
@@ -4,16 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Selector extends Activity {
 
     public RContents items;
+    public Person user;
+    public List<CheckBox> boxes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selector);
+        ScrollView sv = new ScrollView(this);
+        final LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        sv.addView(ll);
+        boxes = new ArrayList<CheckBox>();
+        REntry[] list = items.items;
+        for (int i = 0; i < list.length; i++){
+            CheckBox check = new CheckBox(getApplicationContext());
+            check.setText(list[i].getName()+"   "+list[i].getPrice());
+            ll.addView(check);
+        }
         Intent getRContents = getIntent();
     }
 
+    public void checkBoxClicked (View view) {
+
+    }
 }
