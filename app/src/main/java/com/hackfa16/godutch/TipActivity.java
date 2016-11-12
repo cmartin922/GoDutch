52b50338f6dbbcf934d0984c0408fdef88ed3ce4
saragong14 <sarasgong@gmail.com>
2016-11-11 23:17:42 -0800

    Verifier Layout Created Tip Activity

---
+++ app/src/main/java/com/hackfa16/godutch/TipActivity.java
@@ -0,0 +1,13 @@
+package com.hackfa16.godutch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
