ff3ca82b3d09ab4595ceaa0d8e69e1558caf8159
Danny Wang <dwang@cs.hmc.edu>
2016-11-12 03:27:13 -0800

    added refresh color

--- app/src/main/java/com/hackfa16/godutch/LandingActivity.java
+++ app/src/main/java/com/hackfa16/godutch/LandingActivity.java
@@ -1,9 +1,6 @@
 package com.hackfa16.godutch;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -12,12 +9,8 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.graphics.Bitmap;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
