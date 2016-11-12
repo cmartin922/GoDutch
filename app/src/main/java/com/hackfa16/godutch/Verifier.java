79578ce2bb2d426de662d917c96598062f450377
Andi Chen <konstantinchen@gmail.com>
2016-11-11 21:46:39 -0800

    Re-created Verifier

--- app/src/main/java/com/hackfa16/godutch/Verifier.java
+++ app/src/main/java/com/hackfa16/godutch/Verifier.java
@@ -24,7 +24,7 @@ public class Verifier extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 
 }
