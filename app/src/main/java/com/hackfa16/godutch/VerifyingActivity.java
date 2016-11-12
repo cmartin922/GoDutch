f3fbcbdd0f9e24623ca8c58aec9ff6db9c1ba62c
Andi Chen <konstantinchen@gmail.com>
2016-11-11 23:14:54 -0800

    added methods responding to buttons

--- app/src/main/java/com/hackfa16/godutch/VerifyingActivity.java
+++ app/src/main/java/com/hackfa16/godutch/VerifyingActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
 
 public class VerifyingActivity extends AppCompatActivity {
 
@@ -13,16 +15,15 @@ public class VerifyingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verifying);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        GridView gridview = (GridView) findViewById(R.id.gridView1) ;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void satisfied(View view){
+        // navigate to selector
+    }
+
+    public void notSatisfied(View view){
+
     }
 }
